78b7c08bb0d443fae3a3618d711edd20932d0ab9
WON KIM <kw7383@naver.com>
2023-06-18 00:18:45 +0900

    some change

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+import torch
+
 
 app = FastAPI()
 
+# PyTorch Lightning 체크포인트 파일 경로
+ckpt_path = "path/to/your/checkpoint.ckpt"
+
+# PyTorch Lightning 모델 로드
+model = YourModel.load_from_checkpoint(ckpt_path)
+
 @app.get("/")
 async def read_root():
-    return {"Hello": "World"}
+    # 모델 사용 예시
+    input_tensor = torch.randn(1, 3, 224, 224)
+    output = model(input_tensor)
+    return {"prediction": output.tolist()}
