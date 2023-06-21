77470d7a2c83680eabc7002091ee419000f1cc5c
WON KIM <kw7383@naver.com>
2023-06-21 20:09:08 +0900

    ddd

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 import streamlit as st
-
+from st_files_connection import FilesConnection
 
 st.title("Hello World")
+
+
+conn = st.experimental_connection('s3', type=FilesConnection)
+
+csv = conn.read("s3://mykafkastockbucket/topics/stock-tutorial/test/simple.csv", input_format="csv")
+
+
+
+
+st.write(csv)
