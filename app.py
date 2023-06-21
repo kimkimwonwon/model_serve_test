1f9f5fcb5870a0283fd19d7f24efde4f5da6caf7
WON KIM <kw7383@naver.com>
2023-06-21 20:49:14 +0900

    good

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
 import streamlit as st
 from st_files_connection import FilesConnection
+from modules.vizualization import *
 
-st.title("Hello World")
+st.set_page_config(layout="wide")
+
+if 'stock_name' not in st.session_state:
+    st.session_state['user_name'] = ''
+
+st.title("Market Early 😎")
+
+st.sidebar.title("Market Early 📈")
+
+
+
+st.sidebar.text_input(label="Search Stock", value="AAPL🍎",key="stock_name")
+
+
+# get module from : modules vizualization.py  - > st.write(hi())
+
+st.write(st.session_state.stock_name)
+
+
+
 
 
 conn = st.experimental_connection('s3', type=FilesConnection)
-
 csv = conn.read("s3://mykafkastockbucket/topics/stock-tutorial/test/simple.csv", input_format="csv")
+
+
+
 
 
 
